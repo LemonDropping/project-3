dd7156d3a44f24434bcb40ca884c6d8accf56133
Nicole Marie <nicolependarvis5@gmail.com>
2023-05-10 21:19:08 -0400

    edited login form using mui to style

--- client/classicgames/src/Components/LoginForm.js
+++ client/classicgames/src/Components/LoginForm.js
@@ -1,35 +1,56 @@
 import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    "& > *": {
+      margin: theme.spacing(1),
+      width: "25ch",
+    },
+  },
+  button: {
+    marginTop: theme.spacing(2),
+  },
+}));
 
 const LoginForm = () => {
+  const classes = useStyles();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(`Username: ${username}, Password: ${password}`);
+    // add logic to validate credentials and log user in
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <label>
-        Username:
-        <input
-          type="text"
-          value={username}
-          onChange={(event) => setUsername(event.target.value)}
-        />
-      </label>
-      <br />
-      <label>
-        Password:
-        <input
-          type="password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        />
-      </label>
-      <br />
-      <button type="submit">Log In</button>
+    <form className={classes.root} onSubmit={handleSubmit}>
+      <TextField
+        id="username"
+        label="Username"
+        variant="outlined"
+        value={username}
+        onChange={(event) => setUsername(event.target.value)}
+      />
+      <TextField
+        id="password"
+        label="Password"
+        type="password"
+        variant="outlined"
+        value={password}
+        onChange={(event) => setPassword(event.target.value)}
+      />
+      <Button
+        className={classes.button}
+        variant="contained"
+        color="primary"
+        type="submit"
+      >
+        Log In
+      </Button>
     </form>
   );
 };
